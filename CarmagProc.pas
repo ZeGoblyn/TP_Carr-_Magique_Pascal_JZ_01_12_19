8d43951c14db93d384b13f12a9ad48065b28d172
ZeGoblyn <j.ziegeltrum@ludus-academie.com>
2019-02-01 10:49:58 +0100

    TP Carré magique Joffrey Ziegeltrum Fichier .pas avec et sans procédure + fichier Sublime text pour les algorithmes. Version finales.

---
+++ CarmagProc.pas
@@ -0,0 +1,63 @@
+program carrmag;
+//BUT créer un carré magique
+//ENTREE un tableau vide
+//SORTIE un tableau plain
+
+uses crt;
+
+const taille =5;
+
+var i, x, y: INTEGER;
+	t: array [1..taille, 1..taille] of INTEGER;
+
+PROCEDURE calcul;	
+
+BEGIN
+	y:=2;
+	x:=3;
+
+	FOR i:= 1 to taille*taille DO
+		BEGIN
+		t[x, y]:=i;
+		y := y-1;
+		IF y<1 THEN
+			BEGIN
+			y:=taille;
+			END;
+		x := x+1;
+		IF x> taille THEN
+			BEGIN
+			x:=1;
+			END;
+
+			IF t[x, y] > 0 THEN
+				BEGIN
+				y := y-1;
+				x := x-1;
+				IF x<1 THEN
+					BEGIN
+					x := taille;
+					END;
+					IF y<1 THEN
+					BEGIN
+					y:=taille;
+					END;
+				END;
+		END;
+	END;
+
+
+BEGIN
+	calcul;
+	FOR y:=1 to taille DO
+		BEGIN
+		FOR x:= 1 to taille DO
+		BEGIN
+                write('|');
+		WRITE (t[x, y]);
+		END;
+        write('|');
+        writeln;
+	END;
+	READLN;
+END.
